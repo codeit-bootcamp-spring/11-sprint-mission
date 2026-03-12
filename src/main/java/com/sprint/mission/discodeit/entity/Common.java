7f5e7f4ff1f4b5349526c56eaa0dcda72061fc11
taehk23 <terrypotterk@gmail.com>
2026-03-12 15:31:35 +0900

    refactor: 시간 필드를 Long에서 Instant로 변경

--- src/main/java/com/sprint/mission/discodeit/entity/Common.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Common.java
@@ -3,23 +3,24 @@ package com.sprint.mission.discodeit.entity;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
 public class Common implements Serializable {
     private static final long serialVersionUID = 1L;
     private UUID id;
-    private Long createdAt;
-    private Long updatedAt;
+    private Instant createdAt;
+    private Instant updatedAt;
 
     public Common() {
         this.id = UUID.randomUUID();
-        this.createdAt = System.currentTimeMillis();
-        this.updatedAt = System.currentTimeMillis();
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
     }
 
     public void update() {
-        this.updatedAt = System.currentTimeMillis();
+        this.updatedAt = Instant.now();
     }
 
 }
