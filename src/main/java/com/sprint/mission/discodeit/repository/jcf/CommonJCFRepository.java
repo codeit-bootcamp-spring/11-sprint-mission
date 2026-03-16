326c705c84c28e38e3876fd48df87fab7f82effb
taehk23 <terrypotterk@gmail.com>
2026-03-16 17:07:14 +0900

    refactor: JCF Repository 저장소를 ConcurrentHashMap으로 변경하여 thread-safe 개선

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/CommonJCFRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/CommonJCFRepository.java
@@ -2,45 +2,35 @@ package com.sprint.mission.discodeit.repository.jcf;
 
 import com.sprint.mission.discodeit.entity.Common;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CommonJCFRepository<T extends Common> {
 
-    private List<T> repo;
+    private Map<UUID, T> repo;
 
     public CommonJCFRepository() {
         init();
     }
 
     public void init() {
-        repo = new ArrayList<>();
+        repo = new ConcurrentHashMap<>();
     }
 
     public void save(T obj) {
-        for(int i=0; i<repo.size(); i++) {
-            if(repo.get(i).getId().equals(obj.getId())) {
-                repo.set(i, obj);
-                return;
-            }
-        }
-        repo.add(obj);
+        repo.put(obj.getId(), obj);
     }
 
     public Optional<T> findById(UUID id) {
-        return repo.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst();
+        return Optional.ofNullable(repo.get(id));
     }
 
     public List<T> findAll() {
-        return repo;
+        return new ArrayList<>(repo.values());
     }
 
     public void delete(T obj) {
-        repo.remove(obj);
+        repo.remove(obj.getId());
     }
 
 }
