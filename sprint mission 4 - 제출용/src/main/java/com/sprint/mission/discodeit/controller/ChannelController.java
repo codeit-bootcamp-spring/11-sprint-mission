df6416cf7b2c17abc2b0178025f00dbde8a80d88
tndyd123 <wjdtndyd0001@gmail.com>
2026-03-23 17:23:10 +0900

    feat: ChannelController 구현

--- sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -1,13 +1,68 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.channel.ChannelResponse;
+import com.sprint.mission.discodeit.dto.channel.ChannelUpdateRequest;
+import com.sprint.mission.discodeit.dto.channel.PrivateChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.channel.PublicChannelCreateRequest;
 import com.sprint.mission.discodeit.service.ChannelService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/channel")
 @RequiredArgsConstructor
 public class ChannelController {
     private final ChannelService channelService;
+
+    // public create
+    @RequestMapping(value = "/public", method = RequestMethod.POST)
+    public ResponseEntity <ChannelResponse> createPublicChannel(@RequestBody PublicChannelCreateRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(channelService.createPublicChannel(request));
+    }
+
+    // private create
+    @RequestMapping(value = "/private", method = RequestMethod.POST)
+    public ResponseEntity<ChannelResponse> createPrivateChannel(@RequestBody PrivateChannelCreateRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(channelService.createPrivateChannel(request));
+    }
+
+    // read
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<ChannelResponse> read(@PathVariable UUID id){
+        return ResponseEntity.ok(channelService.read(id));
+    }
+
+    // readAllByUserId
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ChannelResponse>> readAllByUserId(@RequestParam UUID userId){
+        return ResponseEntity.ok(channelService.readAllByUserId(userId));
+    }
+
+    // delete
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        channelService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // update
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@PathVariable UUID id,
+                                       @RequestBody ChannelUpdateRequest request){
+        channelService.update(request);
+        return ResponseEntity.ok().build();
+    }
+
+    // restore
+    @RequestMapping(value = "/{id}/restore", method = RequestMethod.POST)
+    public ResponseEntity<Void> restore(@PathVariable UUID id){
+        channelService.restore(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
