b6205eb7a9fd5e0e8346c4a59c2904383c1e6bd2
tndyd123 <wjdtndyd0001@gmail.com>
2026-03-23 16:20:03 +0900

    feat: AuthController 구현

---
+++ sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -0,0 +1,11 @@
+package com.sprint.mission.discodeit.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/channel")
+@RequiredArgsConstructor
+public class ChannelController {
+}
