c187c0e8845b69ae34989345a7a17fd0d6daeb50
tndyd123 <wjdtndyd0001@gmail.com>
2026-03-23 18:21:43 +0900

    feat: GlobalExceptionHandler 및 ErrorResponse 구현

---
+++ sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.sprint.mission.discodeit.exception;
+
+import java.time.Instant;
+
+// 생성자, getter, equals(), hashCode(), toString() 자동생성
+public record ErrorResponse(
+        // 에러 발생 시각
+        Instant timestamp,
+
+        // HTTP 상태코드 숫자(404)
+        int status,
+
+        // 상태코드 이름(NOT FOUND)
+        String error,
+
+        // 구체적 에러메세지
+        String message
+) {}
--- sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,4 +1,47 @@
 package com.sprint.mission.discodeit.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    //400
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(IllegalArgumentException e) {
+        ErrorResponse response = new ErrorResponse(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Bad Request",
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    // 404
+    @ExceptionHandler(DiscodeitException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(DiscodeitException e) {
+        ErrorResponse response = new ErrorResponse(
+                Instant.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    // 에러코드 500 > 위 두 개에서 잡히지 않은 나머지 모든 예외
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse response = new ErrorResponse(
+                Instant.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Internal Server Error",
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
 }
