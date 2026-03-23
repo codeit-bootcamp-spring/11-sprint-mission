00ad8bf1265de2bf85f09682ed3b7b073b4b44ed
tndyd123 <wjdtndyd0001@gmail.com>
2026-03-23 17:55:54 +0900

    feat: BinaryContentController 구현

--- sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,13 +1,30 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/binarycontent")
 @RequiredArgsConstructor
 public class BinaryContentController {
     private final BinaryContentService binaryContentService;
+
+    // read
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<BinaryContent> read(@PathVariable UUID id){
+        return ResponseEntity.ok(binaryContentService.read(id));
+    }
+
+    // readAllByIdIn
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<BinaryContent>> readAllByIdIn(@RequestParam List<UUID> ids){
+        return ResponseEntity.ok(binaryContentService.readAllByIdIn(ids));
+    }
 }
