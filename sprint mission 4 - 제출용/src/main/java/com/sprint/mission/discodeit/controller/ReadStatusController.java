d456a64d2776a5db0962b681183bab4ab321d381
tndyd123 <wjdtndyd0001@gmail.com>
2026-03-23 17:48:37 +0900

    feat: ReadStatusController 구현

--- sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
+++ sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -1,13 +1,40 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.readStatus.ReadStatusCreateRequest;
+import com.sprint.mission.discodeit.dto.readStatus.ReadStatusUpdateRequest;
+import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.service.ReadStatusService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/readstatus")
 @RequiredArgsConstructor
 public class ReadStatusController {
     private final ReadStatusService readStatusService;
+
+    // create
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<ReadStatus> create(@RequestBody ReadStatusCreateRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(readStatusService.create(request));
+    }
+
+    // readAllByUserId
+    @RequestMapping (method = RequestMethod.GET)
+    public ResponseEntity<List<ReadStatus>> readAllByUserId(@RequestParam UUID userId){
+        return ResponseEntity.ok(readStatusService.readAllByUserId(userId));
+    }
+
+    // update
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@PathVariable UUID id, @RequestBody ReadStatusUpdateRequest request){
+        readStatusService.update(request);
+        return ResponseEntity.ok().build();
+    }
+
 }
