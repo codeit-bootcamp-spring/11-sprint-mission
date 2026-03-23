b9b26a1de593a4806fe966e608511b2c04245665
tndyd123 <wjdtndyd0001@gmail.com>
2026-03-23 14:33:34 +0900

    fix: BaseEntity Serializable 누락 수정

--- sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
+++ sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -2,11 +2,13 @@ package com.sprint.mission.discodeit.entity;
 
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.time.Instant;
 import java.util.UUID;
 
 @Getter
-public class BaseEntity {
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
     protected UUID id;
     protected Instant createdAt;
     protected Instant updatedAt;
