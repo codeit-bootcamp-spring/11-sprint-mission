4e869a3e26820ea9dd2bb166134960b3d0ddc752
pintor.dev <pintor.dev@gmail.com>
2026-03-29 02:58:10 +0900

    feat: add binary content query API

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,10 +1,15 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentResponse;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @RequestMapping("/api/binary-contents")
@@ -12,4 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BinaryContentController {
     private final BinaryContentService binaryContentService;
+
+    @RequestMapping(
+            path = "{binaryContentId}",
+            method = RequestMethod.GET
+    )
+    public ResponseEntity<BinaryContentResponse> findById(
+            @PathVariable UUID binaryContentId
+    ) {
+        BinaryContentResponse binaryContent = this.binaryContentService.findById(binaryContentId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(binaryContent);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET
+    )
+    public ResponseEntity<List<BinaryContentResponse>> findAllByIdsIn(
+            @RequestParam("binaryContentIds") List<UUID> binaryContentIds
+    ) {
+        List<BinaryContentResponse> binaryContents = this.binaryContentService.findAllByIdIn(binaryContentIds);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(binaryContents);
+    }
 }
