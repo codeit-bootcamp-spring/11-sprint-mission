6c97aee21788db081ce31a000fd8b05ed919d85b
pintor.dev <pintor.dev@gmail.com>
2026-04-10 18:18:08 +0900

    feat: implement local disk storage for binary content

---
+++ src/main/java/com/sprint/mission/discodeit/storage/local/LocalBinaryContentStorage.java
@@ -0,0 +1,91 @@
+package com.sprint.mission.discodeit.storage.local;
+
+import com.sprint.mission.discodeit.dto.binarycontent.BinaryContentResponse;
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import com.sprint.mission.discodeit.util.FileLockProvider;
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+import java.util.concurrent.locks.ReentrantLock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@ConditionalOnProperty(name = "discodeit.storage.type", havingValue = "local")
+@Component
+public class LocalBinaryContentStorage implements BinaryContentStorage {
+
+  private final Path root;
+  private final FileLockProvider fileLockProvider;
+
+  public LocalBinaryContentStorage(@Value("${discodeit.storage.local.root-path}") String rootPath,
+      FileLockProvider fileLockProvider) {
+    this.root = Paths.get(rootPath);
+    this.fileLockProvider = fileLockProvider;
+  }
+
+  @PostConstruct
+  public void init() {
+    try {
+      Files.createDirectories(root);
+    } catch (IOException e) {
+      throw new RuntimeException("failed to create root path dir. ❌", e);
+    }
+  }
+
+  @Override
+  public UUID put(UUID id, byte[] bytes) {
+    Path path = this.resolvePath(id);
+    ReentrantLock lock = this.fileLockProvider.getLock(path);
+    lock.lock();
+    try (OutputStream os = Files.newOutputStream(path)) {
+      os.write(bytes);
+    } catch (IOException e) {
+      throw new RuntimeException("failed to store binary content id: " + id + ". ❌", e);
+    } finally {
+      lock.unlock();
+    }
+    return id;
+  }
+
+  @Override
+  public InputStream get(UUID id) {
+    Path path = this.resolvePath(id);
+    try {
+      return Files.newInputStream(path);
+    } catch (IOException e) {
+      return null;
+    }
+  }
+
+  @Override
+  public ResponseEntity<Resource> download(BinaryContentResponse binaryContentResponse) {
+    InputStream inputStream = this.get(binaryContentResponse.id());
+    Resource resource = new InputStreamResource(inputStream);
+
+    return ResponseEntity.ok()
+        .header(HttpHeaders.CONTENT_DISPOSITION,
+            ContentDisposition.attachment()
+                .filename(binaryContentResponse.fileName())
+                .build()
+                .toString())
+        .contentType(MediaType.parseMediaType(binaryContentResponse.contentType()))
+        .contentLength(binaryContentResponse.size())
+        .body(resource);
+  }
+
+  private Path resolvePath(UUID id) {
+    return root.resolve(id.toString());
+  }
+}
