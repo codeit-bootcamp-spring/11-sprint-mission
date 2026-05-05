820636dbbf5499c05a4c8634391f189c73bfb8b4
tndyd123 <wjdtndyd0001@gmail.com>
2026-05-06 05:09:50 +0900

    chore: sprint7-mission 초기 설정

--- sprint mission 4 - 제출용/src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.service;
-
-import com.sprint.mission.discodeit.dto.auth.LoginRequest;
-import com.sprint.mission.discodeit.dto.auth.LoginDto;
-
-public interface AuthService {
-
-  LoginDto login(LoginRequest request);
-}
