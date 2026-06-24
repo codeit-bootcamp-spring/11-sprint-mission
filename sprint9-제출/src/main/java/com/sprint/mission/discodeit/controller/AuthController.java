6400bbb22d74966f5353cb974827524fb52f68ef
tndyd123 <wjdtndyd0001@gmail.com>
2026-06-24 15:13:55 +0900

    feat: 인증 - 세션을 활용한 현재 사용자 정보 조회

--- sprint9-제출/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ sprint9-제출/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,16 @@ public class AuthController implements AuthApi {
     return ResponseEntity
         .status(HttpStatus.NON_AUTHORITATIVE_INFORMATION)
         .build();
+  }
+
+  @GetMapping("me")
+  public ResponseEntity<String> me(
+      @AuthenticationPrincipal DiscodeitUserDetails userDetails
+  ) {
+    UserDto userDto = userDetails.getUserDto();
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(userDto.username());
   }
 }
