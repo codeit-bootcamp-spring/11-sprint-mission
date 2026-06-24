93a7073b2f50455d9343eb4696a75e92c77118eb
tndyd123 <wjdtndyd0001@gmail.com>
2026-06-24 11:33:54 +0900

    feat: 회원가입 비밀번호 암호화

--- sprint9-제출/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ sprint9-제출/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -7,11 +7,12 @@ import com.sprint.mission.discodeit.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
@@ -24,11 +25,11 @@ public class AuthController implements AuthApi {
 
   private final AuthService authService;
 
-  @PostMapping(path = "login")
-  public ResponseEntity<UserDto> login(@RequestBody @Valid LoginRequest loginRequest) {
+  @Override
+  @PostMapping(path = "login", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<UserDto> login(@ModelAttribute @Valid LoginRequest loginRequest) {
     log.info("로그인 요청: username={}", loginRequest.username());
     UserDto user = authService.login(loginRequest);
-    log.debug("로그인 응답: {}", user);
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(user);
