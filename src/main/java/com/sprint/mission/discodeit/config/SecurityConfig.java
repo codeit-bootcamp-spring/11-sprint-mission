5c0ddb95a550f47f3ed4fa3f90cf8057ad36ac5d
김지성 <shes3332@naver.com>
2026-06-25 09:32:53 +0900

    refactor: Spring Security 환경설정 명시적 활성화 및 URL 오타 수정

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.logout.HttpStatusReturnin
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -34,7 +36,7 @@ public class SecurityConfig {
         .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()));
 
     http.formLogin(login -> login
-        .loginProcessingUrl("api/auth/login")
+        .loginProcessingUrl("/api/auth/login")
         .successHandler(loginSuccessHandler)
         .failureHandler(loginFailureHandler)
     );
