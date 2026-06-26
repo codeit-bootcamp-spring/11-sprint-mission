51eeeb81dd54230acc9964a364e351120c15614f
김지성 <shes3332@naver.com>
2026-06-26 10:56:02 +0900

    refactor: UserStatus 관련 잔여 Mapper 코드 정리

--- src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -8,6 +8,6 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", uses = {BinaryContentMapper.class})
 public interface UserMapper {
 
-  @Mapping(target = "online", source = "status.online")
+  @Mapping(target = "online", ignore = true)
   UserDto toDto(User user);
 }
