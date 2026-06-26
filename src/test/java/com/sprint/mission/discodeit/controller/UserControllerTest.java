f09a9a90f73ec9b29ebe2fb31e4a5482b6902249
김지성 <shes3332@naver.com>
2026-06-26 13:04:52 +0900

    test: UserControllerTest 내 UserStatus 관련 코드 정리

--- src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
+++ src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
@@ -15,15 +15,17 @@ import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 
 @WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test")
 public class UserControllerTest {
 
@@ -35,9 +37,6 @@ public class UserControllerTest {
 
   @MockitoBean
   private UserService userService;
-
-  @MockitoBean
-  private UserStatusService userStatusService;
 
   @MockitoBean
   private JpaMetamodelMappingContext jpaMappingContext;
