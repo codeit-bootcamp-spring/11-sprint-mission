8cddcaa65601b1c24dffeda8dd91d6b051b0303c
김지성 <shes3332@naver.com>
2026-07-01 10:49:12 +0900

    feat: 토큰 재발급 예외 처리 추가

---
+++ src/main/java/com/sprint/mission/discodeit/exception/jwt/RefreshTokenInvalidException.java
@@ -0,0 +1,12 @@
+package com.sprint.mission.discodeit.exception.jwt;
+
+import com.sprint.mission.discodeit.exception.DiscodeitException;
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import java.util.Map;
+
+public class RefreshTokenInvalidException extends DiscodeitException {
+
+  public RefreshTokenInvalidException() {
+    super(ErrorCode.REFRESH_TOKEN_INVALID, Map.of());
+  }
+}
