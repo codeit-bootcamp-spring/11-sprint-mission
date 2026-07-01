e169f82a4bb496b4c8df61237311d852b36953ce
김지성 <shes3332@naver.com>
2026-07-01 10:14:48 +0900

    feat: JWT 인증 필터 핵심 로직 구현

---
+++ src/main/java/com/sprint/mission/discodeit/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.sprint.mission.discodeit.security.filter;
+
+import com.sprint.mission.discodeit.security.DiscodeitUserDetailsService;
+import com.sprint.mission.discodeit.security.jwt.JwtTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private final JwtTokenProvider jwtTokenProvider;
+  private final DiscodeitUserDetailsService userDetailsService;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    String authHeader = request.getHeader("Authorization");
+
+    if (authHeader == null || !authHeader.startsWith("Bearer")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    String token = authHeader.substring(7);
+
+    if (jwtTokenProvider.validateToken(token)) {
+      String subject = jwtTokenProvider.getSubject(token);
+      UserDetails userDetails = userDetailsService.loadUserByUsername(subject);
+
+      UsernamePasswordAuthenticationToken authentication =
+          new UsernamePasswordAuthenticationToken(
+              userDetails, null, userDetails.getAuthorities()
+          );
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+    filterChain.doFilter(request, response);
+  }
+}
