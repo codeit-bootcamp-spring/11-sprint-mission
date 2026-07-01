57d2d0644148562dd0f4f336ace83b0be7c51f7d
김지성 <shes3332@naver.com>
2026-07-01 14:41:15 +0900

    feat: 만료된 JWT 토큰 정리를 위한 스케줄러 적용

--- src/main/java/com/sprint/mission/discodeit/security/jwt/InMemoryJwtRegistry.java
+++ src/main/java/com/sprint/mission/discodeit/security/jwt/InMemoryJwtRegistry.java
@@ -7,6 +7,7 @@ import java.util.Queue;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import org.springframework.scheduling.annotation.Scheduled;
 
 public class InMemoryJwtRegistry implements JwtRegistry {
 
@@ -72,6 +73,7 @@ public class InMemoryJwtRegistry implements JwtRegistry {
     return null;
   }
 
+  @Scheduled(fixedDelay = 1000 * 60 * 5)
   @Override
   public void clearExpiredJwtInformation() {
     Instant now = Instant.now();
