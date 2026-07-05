859eff5db51a3bdaa8f42600d1c1a23342f8eb78
pintor.dev <pintor.dev@gmail.com>
2026-07-05 19:00:27 +0900

    feat: replace SessionManager with JwtRegistry for online status check in UserMapper

--- src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -2,7 +2,7 @@ package com.sprint.mission.discodeit.mapper;
 
 import com.sprint.mission.discodeit.dto.user.UserResponse;
 import com.sprint.mission.discodeit.entity.User;
-import com.sprint.mission.discodeit.security.SessionManager;
+import com.sprint.mission.discodeit.security.jwt.JwtRegistry;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 public abstract class UserMapper {
 
   @Autowired
-  protected SessionManager sessionManager;
+  protected JwtRegistry jwtRegistry;
 
   @Mapping(target = "profile", source = "user.profile")
-  @Mapping(target = "online", expression = "java(sessionManager.isOnline(user.getId()))")
+  @Mapping(target = "online", expression = "java(jwtRegistry.hasActiveJwtInformationByUserId(user.getId()))")
   public abstract UserResponse toResponse(User user);
 }
