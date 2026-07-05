d9d987e394dd561460c72f0d1dabe39126feef34
김지성 <shes3332@naver.com>
2026-07-05 23:45:39 +0900

    feat: 트랜잭션 분리를 위한 BinaryContentEventListener 구현 및 상태 업데이트 연동

---
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.event;
+
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class BinaryContentEventListener {
+
+  private final BinaryContentStorage binaryContentStorage;
+  private final BinaryContentService binaryContentService;
+
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void handleBinaryContentCreatedEvent(BinaryContentCreatedEvent event) {
+    log.info("바이너리 파일 스토리지 업로드 시작 - fileId: {}", event.getBinaryContentId());
+
+    try {
+      binaryContentStorage.put(event.getBinaryContentId(), event.getBytes());
+
+      binaryContentService.updateStatus(event.getBinaryContentId(), BinaryContentStatus.SUCCESS);
+      log.info("바이너리 파일 스토리지 업로드 완료 상태 업데이트 - fileId: {}", event.getBinaryContentId());
+    } catch (Exception e) {
+      log.error("바이너리 파일 스토리지 업로드 중 오류 발생 - fileId: {}", event.getBinaryContentId(), e);
+
+      binaryContentService.updateStatus(event.getBinaryContentId(), BinaryContentStatus.FAIL);
+    }
+  }
+}
