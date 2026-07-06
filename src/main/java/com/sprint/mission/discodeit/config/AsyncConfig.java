49cde6c1ddb98ab665a0f59d057613e0ebc6ee11
김지성 <shes3332@naver.com>
2026-07-07 00:22:59 +0900

    feat: 비동기 처리 활성화 및 ContextCopyingTaskDecorator 적용 스레드 풀 구성

---
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -0,0 +1,26 @@
+package com.sprint.mission.discodeit.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+  @Bean(name = "eventTaskExecutor")
+  public Executor eventTaskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(5);
+    executor.setMaxPoolSize(20);
+    executor.setQueueCapacity(50);
+    executor.setThreadNamePrefix("EventAsync-");
+
+    executor.setTaskDecorator(new ContextCopyingTaskDecorator());
+
+    executor.initialize();
+    return executor;
+  }
+}
