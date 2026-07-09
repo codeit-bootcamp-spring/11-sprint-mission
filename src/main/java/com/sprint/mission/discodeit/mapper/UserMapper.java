5a43e6cf4e2da5eba8db66f223988f2f65edbbf7
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-07-09 17:13:10 +0900

    fix: UserMapper수정 및 SessionManager추가

--- src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.mapper;
 
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.security.SessionManager;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 public abstract class UserMapper {
 
   @Autowired
-  protected OnlineStatusResolver onlineStatusResolver;
+  protected SessionManager sessionManager;
 
-  @Mapping(target = "online", expression = "java(onlineStatusResolver.isOnline(user.getId()))")
+  @Mapping(target = "online", expression = "java(sessionManager.hasActiveSessions(user.getId()))")
   public abstract UserDto toDto(User user);
 }
