5bb0869052a33b44f7e3e6ce0c05b5775837fa8f
김지성 <shes3332@naver.com>
2026-07-13 09:10:38 +0900

    feat: Caffeine 캐시 환경 및 Actuator 설정 추가

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,10 +2,12 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableCaching
 @EnableRetry
 @EnableJpaAuditing
 @EnableScheduling
