3075d66d4a3686054aeb8355eb2864b56afa57cc
pintor.dev <pintor.dev@gmail.com>
2026-07-19 17:30:28 +0900

    feat: reflect binary storage success/failure to BinaryContent status

--- src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
@@ -1,5 +1,7 @@
 package com.sprint.mission.discodeit.event;
 
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
+import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +15,21 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class BinaryContentEventListener {
 
   private final BinaryContentStorage binaryContentStorage;
+  private final BinaryContentService binaryContentService;
 
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void on(BinaryContentCreatedEvent event) {
-    this.binaryContentStorage.put(event.binaryContentId(), event.bytes());
+    log.debug("binary-content upload trial: id={}", event.binaryContentId());
+
+    try {
+      this.binaryContentStorage.put(event.binaryContentId(), event.bytes());
+      this.binaryContentService.updateStatus(event.binaryContentId(), BinaryContentStatus.SUCCESS);
+
+      log.info("binary-content upload success: id={}", event.binaryContentId());
+    } catch (Exception e) {
+      log.error("binary-content upload fail: id={}", event.binaryContentId(), e);
+
+      this.binaryContentService.updateStatus(event.binaryContentId(), BinaryContentStatus.FAIL);
+    }
   }
 }
