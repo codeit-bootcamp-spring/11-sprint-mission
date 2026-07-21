b56bca810e0d630661f26ccebb8b35f845345e1d
김지성 <shes3332@naver.com>
2026-07-21 10:27:35 +0900

    feat: 웹소켓 환경 구성 및 STOMP 메시지 브로커 설정 추가

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -92,6 +92,7 @@ public class SecurityConfig {
         .requestMatchers(HttpMethod.POST, "/api/auth/logout").permitAll()
         .requestMatchers(HttpMethod.POST, "/api/auth/refresh").permitAll()
         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+        .requestMatchers("/ws/**").permitAll()
         .anyRequest().authenticated()
     );
 
