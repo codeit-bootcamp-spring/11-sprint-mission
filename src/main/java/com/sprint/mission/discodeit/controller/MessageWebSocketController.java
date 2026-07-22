29bb6cb0a6cc7b65c20eddf582678ca69e2d7578
김지성 <shes3332@naver.com>
2026-07-22 11:33:34 +0900

    feat: STOMP 웹소켓 메시지 바인딩을 위한 @Payload 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/controller/MessageWebSocketController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageWebSocketController.java
@@ -3,9 +3,11 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.MessageCreateRequest;
 import com.sprint.mission.discodeit.service.MessageService;
 import jakarta.validation.Valid;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 
@@ -18,9 +20,9 @@ public class MessageWebSocketController {
   private final MessageService messageService;
 
   @MessageMapping("/messages")
-  public void handleMassage(@Valid MessageCreateRequest request) {
+  public void handleMassage(@Valid @Payload MessageCreateRequest request) {
     log.debug("STOMP 웹소켓 메시지 수신 - channelId: {}", request.channelId());
-    messageService.create(request, null);
+    messageService.create(request, Collections.emptyList());
     log.info("STOMP 웹소켓 메시지 처리 완료 - authorId: {}", request.authorId());
   }
 }
