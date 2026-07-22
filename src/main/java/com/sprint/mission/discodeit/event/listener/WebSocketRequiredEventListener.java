1aeddfde11ccb44d9092e67d894efeb2b3b8c459
김지성 <shes3332@naver.com>
2026-07-22 16:09:04 +0900

    feat: 분산 환경 실시간 통신 한계 극복을 위한 Kafka 기반 글로벌 브로드캐스팅 아키텍처 도입

--- src/main/java/com/sprint/mission/discodeit/event/listener/WebSocketRequiredEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/WebSocketRequiredEventListener.java
@@ -3,6 +3,8 @@ package com.sprint.mission.discodeit.event.listener;
 import com.sprint.mission.discodeit.event.MessageCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -14,15 +16,23 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class WebSocketRequiredEventListener {
 
   private final SimpMessagingTemplate messagingTemplate;
+  private final KafkaTemplate<String, Object> kafkaTemplate;
+  private static final String TOPIC = "discodeit.MessageCreatedEvent";
 
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-  public void handleMessage(MessageCreatedEvent event) {
-    log.debug("웹소켓 메시지 브로드캐스팅 준비 - channelId: {}, messageId: {}", event.getChannelId(),
+  public void publishToKafka(MessageCreatedEvent event) {
+    log.debug("Kafka 토픽으로 메시지 브로드캐스팅 발행 준비 - channelId: {}, messageId: {}", event.getChannelId(),
         event.getMessageId());
 
+    kafkaTemplate.send(TOPIC, event);
+  }
+
+  @KafkaListener(topics = TOPIC, groupId = "#{T(java.util.UUID).randomUUID().toString()}")
+  public void consumeFromKafkaAndBroadcast(MessageCreatedEvent event) {
     String destination = "/sub/channels." + event.getChannelId() + ".messages";
+
     messagingTemplate.convertAndSend(destination, event);
 
-    log.info("웹소켓 브로드캐스팅 완료 - 목적지: {}", destination);
+    log.info("Kafka 이벤트 수신 후 웹소켓 로컬 브로드캐스팅 완료 - 목적지: {}", destination);
   }
 }
