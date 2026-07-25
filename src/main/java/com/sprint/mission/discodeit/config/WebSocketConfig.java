b4acd939f631b88b9f6111e720f089d2c9c20d35
kim gwanhee <rhksgml55@gmail.com>
2026-07-25 22:47:57 +0900

    feat: STOMP 웹소켓 CONNECT 시 JWT 인증 적용

--- src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,14 +1,21 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.security.jwt.JwtAuthenticationChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtAuthenticationChannelInterceptor jwtAuthenticationChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -19,5 +26,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(
+                jwtAuthenticationChannelInterceptor, new SecurityContextChannelInterceptor()
+        );
     }
 }
